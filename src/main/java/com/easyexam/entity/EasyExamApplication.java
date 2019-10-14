4e3a66445b5891d4c71679a3e36522c90350b1df
fwz <fanweizhi0604@gmail.com>
2019-10-14 10:10:21 +0800

    配置springboot相关设置

--- src/main/java/com/easyexam/entity/EasyExamApplication.java
+++ src/main/java/com/easyexam/entity/EasyExamApplication.java
@@ -1,9 +1,11 @@
 package com.easyexam.entity;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.easyexam.entity.dao")
 public class EasyExamApplication {
 
     public static void main(String[] args) {
