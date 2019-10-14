164208e66d90affe38a1d13057d896b98f835a3a
fwz <fanweizhi0604@gmail.com>
2019-10-14 09:25:51 +0800

    Initial commit

---
+++ src/main/java/com/easyexam/entity/EasyExamApplication.java
@@ -0,0 +1,13 @@
+package com.easyexam.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyExamApplication.class, args);
+    }
+
+}
