976eba832b36eaba3606dd85a8439363942b4c2d
bairuisen <bairuisen@aliyun.com>
2019-10-20 18:44:01 +0800

    整合了系统管理功能

--- src/main/java/com/easyexam/apps/controller/PermissionController.java
+++ src/main/java/com/easyexam/apps/controller/PermissionController.java
@@ -6,47 +6,59 @@ import com.easyexam.apps.entity.PermissionTree;
 import com.easyexam.apps.service.PermissionService;
 import com.github.pagehelper.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RestController      //操作权限Permission
+@Controller     //操作权限Permission
 public class PermissionController {
 
     @Autowired
     private PermissionService permissionService;
+
+    @RequestMapping("findAllPermission")
+    public String findAllPermission1() {
+        return "permissionAdmin";
+    }
+
     @RequestMapping("/findAllPermission.do")
-    public Map<String,Object> findAllPermission(Integer page, Integer limit){
+    @ResponseBody
+    public Map<String, Object> findAllPermission(Integer page, Integer limit) {
         List<Permission> list = permissionService.findAll(page, limit);
         Map<String, Object> map = new HashMap<>();
-        map.put("code",0);
-        map.put("count",((Page)list).getTotal());
-        map.put("data",list);
+        map.put("code", 0);
+        map.put("count", ((Page) list).getTotal());
+        map.put("data", list);
         return map;
     }
 
     @RequestMapping("/addPermission.do")
-    public JsonResult addPermission(Permission permission){
+    @ResponseBody
+    public JsonResult addPermission(Permission permission) {
         return permissionService.addPermission(permission);
     }
 
+    @ResponseBody
     @RequestMapping("/deletePermission.do")
-    public JsonResult deletePermission(Integer pid){
+    public JsonResult deletePermission(Integer pid) {
         return permissionService.deleteOnePermission(pid);
     }
 
+    @ResponseBody
     @RequestMapping("/updatePermission.do")
-    public JsonResult updatePermission(Permission permission){
+    public JsonResult updatePermission(Permission permission) {
         return permissionService.updatePermission(permission);
     }
 
+    @ResponseBody
     @RequestMapping("/findZtr.do")
-    public JsonResult findZtr(){
+    public JsonResult findZtr() {
         List<PermissionTree> list = permissionService.findZtr();
-        return new JsonResult(1,list);
+        return new JsonResult(1, list);
     }
 
 
--- src/main/java/com/easyexam/apps/controller/RoleController.java
+++ src/main/java/com/easyexam/apps/controller/RoleController.java
@@ -14,13 +14,18 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@ResponseBody
 public class RoleController {
 
     @Autowired
     private RoleService roleService;
 
+    @RequestMapping("/findAllRole")
+    public String findAllRole1(){
+        return "roleAdmin";
+    }
+
     @RequestMapping("/findAllRole.do")     //查找所有角色
+    @ResponseBody
     public Map<String, Object> findAllRole(){
         List<Role> list = roleService.findAllRole();
         Map<String, Object> map = new HashMap<>();
@@ -30,22 +35,26 @@ public class RoleController {
     }
 
     @RequestMapping("/addRole.do")  //添加角色
+    @ResponseBody
     public JsonResult addRole(Role role){
         List<Integer> pid = role.getPid();
         return roleService.addRole(role,pid);
     }
 
     @RequestMapping("/deleteRole.do")  //删除角色   注意角色关联
+    @ResponseBody
     public JsonResult deleteRole(Integer rid){
         return roleService.deleteRole(rid);
     }
 
     @RequestMapping("/updateRole.do")   //修改角色信息
+    @ResponseBody
     public JsonResult updateRole(Role role){
         return roleService.updateRole(role);
     }
 
     @RequestMapping("/findRoleTree.do")  //获得角色树
+    @ResponseBody
     public Map<String, Object> findRoleTree(){
         List<RoleTree> list = roleService.findRoleTree();
         Map<String, Object> map = new HashMap<>();
